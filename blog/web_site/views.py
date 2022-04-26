ce59fb8df64fcbca33f053c428ef3ee7560b93b1
khasanovmma <khasanovmma010@gmail.com>
2022-04-26 16:48:00 +0500

    v1.4 web_site templates end

--- blog/web_site/views.py
+++ blog/web_site/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render, HttpResponse
 from .models import Post
 
 def home_page(request):
-
-    return render(request, 'web_site/index.html')
-
+    return render(request, 'web_site/blog.html')
 
 def blog_page(request):
     return render(request, 'web_site/blog.html')
