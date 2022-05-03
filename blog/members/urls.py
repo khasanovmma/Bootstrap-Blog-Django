a038edb5dd364462f62615557b403abeaa022702
KhasanovMMA <KhasanovMMA010@gmail.com>
2022-05-03 17:04:24 +0500

    v1.5 members templates register-form

--- blog/members/urls.py
+++ blog/members/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login/', login_form, name='login_form'),
+    path('login-form/', login_form, name='login_form'),
+    path('register-form/', register_form, name='register_form'),
 ]
--- blog/members/views.py
+++ blog/members/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def login_form(request):
     return render(request, 'members/login_form.html')
+
+def register_form(request):
+    return render(request, 'members/register_form.html')
