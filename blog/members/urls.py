a4a30753d39652c6303e9429c6f6e53ff2af5460
khasanovmma <khasanovmma010@gmail.com>
2022-05-04 21:08:25 +0500

    v1.9 members update

--- blog/members/urls.py
+++ blog/members/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
+    path('edit_prodile/', UserEditView.as_view(), name="edit_profile"),
 ]
--- blog/members/views.py
+++ blog/members/views.py
@@ -1,17 +1,27 @@
 from re import template
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.messages.views import SuccessMessageMixin
 from .forms import SignUpForm
 
 
-class UserRegisterView(CreateView):
+class UserRegisterView(SuccessMessageMixin, CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+    success_message = 'Registration was successfully completed'
     
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['invalide'] = 'is-invalide'
-        return context
+class UserEditView(UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
+    
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['invalide'] = 'is-invalide'
+    #     return context
