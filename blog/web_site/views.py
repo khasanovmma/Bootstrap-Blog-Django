af1f2b4be3908eba98bbd7f1423f87667851c84c
khasanovmma <khasanovmma010@gmail.com>
2022-05-28 16:53:18 +0500

    v1.10.7 update template index

--- blog/web_site/views.py
+++ blog/web_site/views.py
@@ -10,6 +10,18 @@ class HomePageView(ListView):
     template_name = 'web_site/index.html'
     
 
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
+
 def blog_page(request):
     return render(request, 'web_site/blog.html')
 
