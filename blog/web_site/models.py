7a5ecfbe7715d9aa385896d9a76baf8c213f9b7f
khasanovmma <khasanovmma010@gmail.com>
2022-05-28 16:42:07 +0500

    v1.10.6 migrate

--- blog/web_site/models.py
+++ blog/web_site/models.py
@@ -71,7 +71,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.OneToOneField(User, on_delete=models.CASCADE)
-    body = models.CharField()(max_length=255)
+    body = models.CharField(max_length=255)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
