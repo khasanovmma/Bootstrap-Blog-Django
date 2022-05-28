03adcd0236d87eff0f88d54a269cdcec13cd4ba6
khasanovmma <khasanovmma010@gmail.com>
2022-05-28 16:29:50 +0500

    v1.10.3 update templatetags

--- blog/web_site/templatetags/website_tags.py
+++ blog/web_site/templatetags/website_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from web_site.models import Profile
+from web_site.models import Profile, Post
 
 register = template.Library()
 
@@ -13,3 +13,8 @@ def user_profile(request):
 def user_profile_image(user_pk):
     profile = Profile.objects.get(user=user_pk)
     return profile.image_url
+    
+@register.simple_tag()
+def get_latest_post():
+    posts = Post.objects.all().order_by('-id')[:3]
+    return posts
