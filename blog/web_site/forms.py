3dddf326324273a0774def0e741a3f4e50cd5c47
khasanovmma <khasanovmma010@gmail.com>
2022-05-31 20:06:16 +0500

    v1.10.8 forms.py website

--- blog/web_site/forms.py
+++ blog/web_site/forms.py
@@ -15,3 +15,23 @@ class PostForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'category', 'content',  'photo')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'This is Title Placeholder'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=Category, attrs={'class': 'form-control'}),
+        }
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'body')
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
